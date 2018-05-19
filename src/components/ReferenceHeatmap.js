497b56da6d28166ffb4f6d10d229cda376baf846
James Hadfield <jh22@sanger.ac.uk>
2018-05-19 10:34:46 -0700

    heatmap styling

--- src/components/ReferenceHeatmap.js
+++ src/components/ReferenceHeatmap.js
@@ -3,20 +3,33 @@ import { select } from "d3-selection";
 import { rgb } from "d3-color";
 import { interpolateHcl } from "d3-interpolate";
 import { scaleLinear } from "d3-scale";
-import {calcScales, drawAxes} from "../utils/commonFunctions";
+import {calcScales} from "../utils/commonFunctions";
 import {chartTitleCSS} from "../utils/commonStyles";
 
 /* given the DOM dimensions of the chart container, calculate the chart geometry (used by the SVG & D3) */
 const calcChartGeom = (DOMRect) => ({
   width: DOMRect.width,
   height: DOMRect.height - 20, // title line
-  spaceLeft: 40,
+  spaceLeft: 60,
   spaceRight: 10,
   spaceBottom: 60,
   spaceTop: 10
 });
 
-const drawHeatMap = (svg, chartGeom, scales, cfData, colourScale) => {
+const calcCellDims = (chartGeom, cfData) => {
+  const cellPadding = 1;
+  const availableWidth = chartGeom.width - chartGeom.spaceLeft - chartGeom.spaceRight;
+  const availableHeight = chartGeom.height - chartGeom.spaceBottom - chartGeom.spaceTop;
+  const cellWidth = availableWidth / cfData.length - cellPadding;
+  const cellHeight = availableHeight / cfData[0].length - cellPadding;
+  return {
+    height: cellHeight,
+    width: cellWidth,
+    padding: cellPadding
+  }
+}
+
+const drawHeatMap = (svg, chartGeom, scales, cellDims, cfData, colourScale) => {
   /* step1 : flatten cfData */
   /* data point structure for d3: [channel # (1-based),  ref idx (1-based), ref match % (over [0, 100])] */
   const data = cfData.reduce((acc, laneData, laneIdx) => {
@@ -25,17 +38,66 @@ const drawHeatMap = (svg, chartGeom, scales, cfData, colourScale) => {
     return acc.concat(points)
   }, []);
 
+  svg.selectAll(".refLabel")
+      .data(cfData[0].map((x) => x.key)) /* get the labels */
+      .enter()
+      .append("text")
+      .attr("class", "refLabel")
+      .text((d) => d)
+      .attr('y', (d, i) => scales.y(i+1) + 0.5*cellDims.height) /* +1 as that's what we do in the data reduction above */
+      .attr('x', chartGeom.spaceLeft - 2)
+      .attr("text-anchor", "end")
+      .attr("font-size", "16px")
+      .attr("alignment-baseline", "middle") /* i.e. y value specifies top of text */
+
+  svg.selectAll(".channel")
+      .data(cfData.map((d, i) => i+1)) /* get the channels - +1 as per data reduce above */
+      .enter()
+      .append("text")
+      .attr("class", "channel")
+      .text((d) => d)
+      .attr('x', (d) => scales.x(d) - 0.5*cellDims.width)
+      .attr('y', chartGeom.height - chartGeom.spaceBottom + 5)
+      .attr("text-anchor", "middle")
+      .attr("font-size", "16px")
+      .attr("alignment-baseline", "hanging")
+
   svg.selectAll(".heat").remove();
-  const cellSize = 20;
   svg.selectAll(".heat")
     .data(data)
     .enter().append("rect")
     .attr("class", "heat")
-    .attr('width', cellSize)
-    .attr('height', cellSize)
-    .attr("x", d => scales.x(d[0]-1))
-    .attr("y", d => scales.y(d[1]))
+    .attr('width', cellDims.width)
+    .attr('height', cellDims.height)
+    .attr("x", d => scales.x(d[0]-1) + cellDims.padding)
+    .attr("y", d => scales.y(d[1]) + cellDims.padding)
     .attr("fill", d => colourScale(d[2]));
+
+
+  const legendDataValues = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+  const legendBoxWidth = (chartGeom.width - chartGeom.spaceLeft - chartGeom.spaceRight) / (legendDataValues.length -1);
+  const legendBoxHeight = 12;
+  const legendRoof = chartGeom.height - chartGeom.spaceBottom + 32;
+
+  const legend = svg.selectAll(".legend")
+    .data(legendDataValues.slice(0, legendDataValues.length-1)) /* don't include the last one... */
+    .enter().append("g")
+      .attr("class", "legend")
+
+  legend.append("rect")
+    .attr('y', legendRoof)
+    .attr("x", (d, i) => chartGeom.spaceLeft + legendBoxWidth * i)
+    .attr("width", legendBoxWidth)
+    .attr("height", legendBoxHeight)
+    .style("fill", (d) => colourScale(d));
+
+  legend.append("text")
+    .text((d, i) => i ? d+"%" : "")
+    .attr('x', (d, i) => chartGeom.spaceLeft + legendBoxWidth * i)
+    .attr('y', legendRoof + legendBoxHeight + 2)
+    .attr("text-anchor", "middle")
+    .attr("font-size", "12px")
+    .attr("alignment-baseline", "hanging")
 }
 
 class ReferenceHeatmap extends React.Component {
@@ -44,29 +106,28 @@ class ReferenceHeatmap extends React.Component {
     this.state = {chartGeom: {}};
   }
   componentDidMount() {
-    const newState = {
-      SVG: select(this.DOMref),
-      chartGeom: calcChartGeom(this.boundingDOMref.getBoundingClientRect())
-    }
+    const SVG = select(this.DOMref);
+    const chartGeom = calcChartGeom(this.boundingDOMref.getBoundingClientRect());
+    const cellDims = calcCellDims(chartGeom, this.props.refMatchPerChannel);
+
     /* PS the scales never need updating :) */
-    newState.scales = calcScales(
-      newState.chartGeom,
+    const scales = calcScales(
+      chartGeom,
       this.props.refMatchPerChannel.length,
       this.props.refMatchPerChannel[0].length
     );
-    newState.heatColourScale = scaleLinear()
+    const heatColourScale = scaleLinear()
       .domain([0, 100])
       // .range(heatColours)
       .interpolate(interpolateHcl)
       .range([rgb("#007AFF"), rgb('#FFF500')]);
-    drawAxes(newState.SVG, newState.chartGeom, newState.scales)
-    drawHeatMap(newState.SVG, newState.chartGeom, newState.scales, this.props.refMatchPerChannel, newState.heatColourScale)
-    this.setState(newState);
+    drawHeatMap(SVG, chartGeom, scales, cellDims, this.props.refMatchPerChannel, heatColourScale)
+    this.setState({SVG, chartGeom, cellDims, scales, heatColourScale});
   }
 
   componentDidUpdate(prevProps) {
     if (prevProps.version !== this.props.version) {
-      drawHeatMap(this.state.SVG, this.state.chartGeom, this.state.scales, this.props.refMatchPerChannel, this.state.heatColourScale)
+      drawHeatMap(this.state.SVG, this.state.chartGeom, this.state.scales, this.state.cellDims, this.props.refMatchPerChannel, this.state.heatColourScale)
     }
   }
   render() {
