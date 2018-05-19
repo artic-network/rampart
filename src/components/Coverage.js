659b4d318aac23c9b5ac35856d74064e3a21607a
James Hadfield <jh22@sanger.ac.uk>
2018-05-18 17:06:24 -0700

    begin genome annotation display

--- src/components/Coverage.js
+++ src/components/Coverage.js
@@ -26,29 +26,59 @@ export const drawCurve = (svg, chartGeom, scales, data, colours) => {
   }
 }
 
+
+/* draw the genes (annotations) */
+const drawGenes = (svg, chartGeom, scales, annotation) => {
+  svg.selectAll(".annotation").remove();
+  const geneHeight = 15;
+  const spaceAvailable = chartGeom.spaceBottom - 10; /* allow for x labels */
+  const geneYLinePositiveStrand = chartGeom.height - spaceAvailable + geneHeight;
+
+  const genes = annotation.genes
+  const geneNames = Object.keys(annotation.genes);
+
+  const genesSel = svg.selectAll(".gene")
+    .data(geneNames)
+    .enter()
+    .append("g");
+
+  genesSel.append("rect")
+    .attr("class", "gene")
+    .attr("x", (name) => {console.log(name); return scales.x(genes[name].start)})
+    .attr("y", (name) => genes[name].strand === 1 ? geneYLinePositiveStrand : geneYLinePositiveStrand-geneHeight)
+    .attr("width", (name) => scales.x(genes[name].end) - scales.x(genes[name].start))
+    .attr("height", geneHeight)
+    .style("fill", "none")
+    .style("stroke", "black");
+
+  genesSel.append("text")
+    .attr("x", (name) => scales.x(genes[name].start) + (scales.x(genes[name].end) - scales.x(genes[name].start))/2)
+    .attr("y", (name) => genes[name].strand === 1 ? geneYLinePositiveStrand : geneYLinePositiveStrand-geneHeight)
+    .attr("dy", ".9em")
+    .attr("text-anchor", "middle")
+    .attr("alignment-baseline", "ideographic")
+    .style("fill", "black")
+    .text((name) => name);
+};
+
+
 /* given the DOM dimensions of the chart container, calculate the chart geometry (used by the SVG & D3) */
 const calcChartGeom = (DOMRect) => ({
   width: DOMRect.width,
   height: DOMRect.height - 20, // title line
   spaceLeft: 40,
   spaceRight: 10,
-  spaceBottom: 20,
+  spaceBottom: 60,
   spaceTop: 10
 });
 
-const getCoverageMaxes = (coveragePerChannel) => {
-  /* do this in app and only check the "new" data to change things... */
-  const maxX = coveragePerChannel.reduce((acc, channelData) => {
-    return channelData.slice(-1)[0].key;
-  }, 0)
-  const maxY = coveragePerChannel.reduce((outerAcc, channelData) => {
+const getMaxCoverage = (coveragePerChannel) =>
+  coveragePerChannel.reduce((outerAcc, channelData) => {
     const channelMax = channelData.reduce((innerAcc, point) => {
       return point.value > innerAcc ? point.value : innerAcc;
     }, 0);
     return channelMax > outerAcc ? channelMax : outerAcc;
   }, 0)
-  return [maxX, maxY];
-}
 
 class CoveragePlot extends React.Component {
   constructor(props) {
@@ -56,15 +86,16 @@ class CoveragePlot extends React.Component {
     this.state = {chartGeom: {}};
   }
   componentDidMount() {
-    console.log(this.props.annotation)
+    console.log("ANN", this.props.annotation)
     const newState = {
       SVG: select(this.DOMref),
       chartGeom: calcChartGeom(this.boundingDOMref.getBoundingClientRect())
     }
-    newState.scales = calcScales(newState.chartGeom, ...getCoverageMaxes(this.props.coveragePerChannel));
+    newState.scales = calcScales(newState.chartGeom, this.props.annotation.genome.length, getMaxCoverage(this.props.coveragePerChannel));
     drawAxes(newState.SVG, newState.chartGeom, newState.scales)
     drawCurve(newState.SVG, newState.chartGeom, newState.scales, this.props.coveragePerChannel, this.props.colours)
     this.setState(newState);
+    drawGenes(newState.SVG, newState.chartGeom, newState.scales, this.props.annotation)
   }
 
   componentDidUpdate(prevProps) {
@@ -72,9 +103,9 @@ class CoveragePlot extends React.Component {
       const newState = {
         scales: this.state.scales
       };
-      const coverageMaxes = getCoverageMaxes(this.props.coveragePerChannel);
-      if (haveMaxesChanged(this.state.scales, ...coverageMaxes)) {
-        newState.scales = calcScales(this.state.chartGeom, ...coverageMaxes);
+      const coverageMax = getMaxCoverage(this.props.coveragePerChannel);
+      if (haveMaxesChanged(this.state.scales, this.props.annotation.genome.length, coverageMax)) {
+        newState.scales = calcScales(this.state.chartGeom, this.props.annotation.genome.length, coverageMax);
         drawAxes(this.state.SVG, this.state.chartGeom, newState.scales)
       }
       drawCurve(this.state.SVG, this.state.chartGeom, newState.scales, this.props.coveragePerChannel, this.props.colours)
