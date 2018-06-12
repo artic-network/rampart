2776c9363aaa56c1e12fc75be085aeb4abea5542
Andrew Rambaut <rambaut@gmail.com>
2018-06-13 00:38:43 +0100

    updated read mapper

--- scripts/read_mapping_daemon.py
+++ scripts/read_mapping_daemon.py
@@ -1,5 +1,9 @@
+import os
 import sys
 import time  
+from collections import deque
+import threading
+
 from watchdog.observers import Observer  
 from watchdog.events import PatternMatchingEventHandler
 import mappy as mp
@@ -22,6 +26,8 @@ def map_to_reference(aligner, query_file, channel_name, destination_folder):
 	global read_mappings
 	
 	print("Mapping query file: " + query_file);
+	
+	i = 0
 	
 	for name, seq, qual, comment in mp.fastx_read(query_file, read_comment=True): # read one sequence
 		read_time = re.search(r'start_time=([^\s]+)', comment).group(1)
@@ -53,17 +59,48 @@ def map_to_reference(aligner, query_file, channel_name, destination_folder):
 				count = 0
 		except:
 			print(name + " unmatched")
+		
+		i += 1	
+	
+	print("Finished mapping " + str(i) + " reads in file: " + query_file);
 
-
-class MyHandler(PatternMatchingEventHandler):
-	patterns = ["*.fastq", "*.fasta"]
+# This thread class watches the file deque and if there are two or more files
+# in it then it takes the oldest one and maps it (if there is only one file
+# in the deque then it may not have finished writing).
+class Mapper(threading.Thread):
 	aligner = None
+	channel_name = ""
 	destination_folder = ""
-
-	def __init__(self, aligner, channel_name, destination_folder, *args, **kwargs):
+	file_queue = None
+	
+	def __init__(self, aligner, channel_name, destination_folder, file_queue):
 		self.aligner = aligner
+		self.channel_name = channel_name
 		self.destination_folder = destination_folder
-		super(MyHandler, self).__init__(*args, **kwargs)
+		self.file_queue = file_queue
+
+		threading.Thread.__init__(self)
+        
+	def run (self):
+		while True:
+			# if there is more than one file in the deque then the first one must
+			# have finished writing so is ready to map
+			if len(file_queue) > 1:
+				file = file_queue.popleft()
+			
+				print("Popping " + file + " from queue - mapping")
+				map_to_reference(aligner, file, channel_name, destination_folder)				
+
+# The Watcher watches the source folder and if a file is created then it pops
+# its name into the deque for the mapping thread to deal with.
+class Watcher(PatternMatchingEventHandler):
+	patterns = ["*.fastq", "*.fasta"]
+	file_queue = None
+
+	def __init__(self, file_queue, *args, **kwargs):
+		self.file_queue = file_queue
+		
+		super(Watcher, self).__init__(*args, **kwargs)
 
 	def process(self, event):
 		"""
@@ -75,10 +112,11 @@ class MyHandler(PatternMatchingEventHandler):
 			path/to/observed/file
 		"""
 		# the file will be processed there
-		print(event.src_path, event.event_type)  
+		#print(event.src_path, event.event_type)  
 		 
 		if event.event_type == 'created':
-			map_to_reference(aligner, event.src_path, channel_name, destination_folder)
+			print("Appending " + event.src_path + " to queue")
+			file_queue.append(event.src_path)
 
 	def on_modified(self, event):
 		self.process(event)
@@ -96,8 +134,14 @@ if __name__ == '__main__':
 
 	aligner = create_index(reference_file)
 	
+	file_queue = deque([])
+	
+	# start the thread that processes files push to the stack
+	mapper = Mapper(aligner, channel_name, destination_folder, file_queue)
+	mapper.start()
+
 	observer = Observer()
-	observer.schedule(MyHandler(aligner, channel_name, destination_folder), source_folder)
+	observer.schedule(Watcher(file_queue), source_folder)
 	observer.start()
 
 	try:
@@ -107,4 +151,4 @@ if __name__ == '__main__':
 		observer.stop()
 
 	observer.join()
-            
+        
