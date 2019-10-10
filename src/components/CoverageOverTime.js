b3c906e4762e5b99a390a858bc9f05021603a6c6
james hadfield <jh22@sanger.ac.uk>
2019-10-10 14:08:39 +1300

    only display coverage labels over 10%

--- src/components/CoverageOverTime.js
+++ src/components/CoverageOverTime.js
@@ -23,7 +23,7 @@ const strokeDashFunction = (i) => { /* i = 0, 1 or 2 */
     return "0, 2";
 };
 
-const drawLines = (svg, scales, data, colour) => {
+const drawProgressLines = (svg, scales, data, colour) => {
     svg.selectAll(".coverageLine").remove();
     svg.selectAll(".coverageLine")
         .data(["over1000x", "over100x", "over10x"])
@@ -44,48 +44,39 @@ const drawLines = (svg, scales, data, colour) => {
 };
 
 const drawMaxLines = (svg, scales, data, colour) => {
-    const labels = ["1000x", "100x", "10x"];
+    const potentialLabels = ["over1000x", "over100x", "over10x"];
+    const timespan = [data[0].time, data[data.length-1].time];
+    /* only want to display labels over 10% else they are too visually cluttered with the x axis */
+    const labels = potentialLabels
+        .map((l, i) => ({key: l, coverage: data[data.length - 1][l], originalIdx: i}))
+        .filter((o) => o.coverage > 10);
 
     svg.selectAll(".maxCoverageLine").remove();
     svg.selectAll(".maxCoverageLine")
-        .data(["over1000x", "over100x", "over10x"])
+        .data(labels)
         .enter().append("path")
         .attr("class", "maxCoverageLine")
         .attr("fill", "none")
         .attr("stroke", colour)
         .attr("stroke-width", 1)
-        .attr('d', (coverageKey) => {
-            const coverage = data[data.length - 1][coverageKey];
-            const firstTime = data[0].time;
-            const lastTime = data[data.length - 1].time;
-            return `M ${scales.x(firstTime)},${scales.y(coverage)} L ${scales.x(lastTime)},${scales.y(coverage)}`;
-        });
+        .attr('d', (d) => 
+            `M ${scales.x(timespan[0])},${scales.y(d.coverage)} L ${scales.x(timespan[1])},${scales.y(d.coverage)}`
+        )
 
     svg.selectAll(".maxCoverageText").remove();
     svg.selectAll(".maxCoverageText")
-        .data(["over1000x", "over100x", "over10x"])
+        .data(labels)
         .enter().append("text")
         .attr("class", "maxCoverageText axis")
-        .attr("x", (coverageKey, index) => {
-            const firstTime = scales.x(data[0].time);
-            const lastTime = scales.x(data[data.length - 1].time);
-            return (index === 0 ? firstTime : (index === 2 ? lastTime : (lastTime - firstTime) / 2));
-        })
-        .attr("y", (coverageKey) => {
-            const coverage = data[data.length - 1][coverageKey];
-            return scales.y(coverage);
-        })
+        .attr("x", (d) => 
+            scales.x(d.originalIdx === 0 ? timespan[0] : (d.originalIdx === 2 ? timespan[1] : ((timespan[1]-timespan[0])/2)))
+        )
+        .attr("y", (d) => scales.y(d.coverage))
         .attr("dy", "12px") /* positive values bump down text */
-        .attr("text-anchor", (coverageKey, index) => {
-            return (index === 0 ? "start" : (index === 2 ? "end" : "center"));
-
-        })
+        .attr("text-anchor", (d) => d.originalIdx === 0 ? "start" : (d.originalIdx === 2 ? "end" : "center"))
         .attr("baseline-shift", "120%") /* i.e. y value specifies top of text */
         .attr("pointer-events", "none") /* don't capture mouse over */
-        .text((coverageKey, index) => {
-            const coverage = data[data.length - 1][coverageKey];
-            return `>${labels[index]} = ${coverage}%`;
-        });
+        .text((d) => `>${d.key} = ${d.coverage}%`)
 };
 
 const drawLegend = (svg, chartGeom, colour) => {
@@ -135,7 +126,7 @@ class CoverageOverTime extends React.Component {
         const timeMax = (parseInt(finalDataPt.time/30, 10) +1) * 30;
         const scales = {x: calcXScale(this.state.chartGeom, timeMax), y: this.state.yScale};
         drawAxes(this.state.svg, this.state.chartGeom, scales, {xTicks: 4, yTicks:5, isTime: true, ySuffix: "%"});
-        drawLines(this.state.svg, scales, this.props.temporalData, this.state.colour);
+        drawProgressLines(this.state.svg, scales, this.props.temporalData, this.state.colour);
         drawMaxLines(this.state.svg, scales, this.props.temporalData, this.state.colour);
     }
     render() {
