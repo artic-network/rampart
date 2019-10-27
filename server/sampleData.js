4826842528676f6385f7e9d77c64c4f3dcaf73b5
Andrew Rambaut <rambaut@gmail.com>
2019-10-27 11:24:55 +0000

    Indexing out by one - gives a rate one step quicker

--- server/sampleData.js
+++ server/sampleData.js
@@ -131,7 +131,7 @@ SampleData.prototype.summariseTemporalData = function(timestampAdjustment) {
     }
 
     for (let i = ret.length - 1; i > 0; i--) {
-        for (let j = i - 1; j > 0; j--) {
+        for (let j = i - 1; j >= 0; j--) {
             if (ret[i].time - ret[j].time > TIME_WINDOW) {
                 ret[i].mappedRate = (ret[i].mappedCount - ret[j].mappedCount) / (ret[i].time - ret[j].time);
             }
