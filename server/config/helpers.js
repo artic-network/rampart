aafae2531098f5c6ddf3841943e7b776073b39d4
Andrew Rambaut <rambaut@gmail.com>
2023-09-30 16:36:45 +0100

    barcode.csv file reader now more intelligent about finding the samples and barcodes columns

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -25,63 +25,63 @@ const { normalizePath, getAbsolutePath, verbose, warn, fatal } = require("../uti
 
 
 function readConfigFile(paths, fileName) {
-  let config = {};
+    let config = {};
 
-  try {
-      // iterate over the paths and if a file exists, read it on top of the
-      // existing configuration object.
-      paths.forEach((path) => {
-          const filePath = normalizePath(path) + fileName;
-          if (fs.existsSync(filePath)) {
-              verbose("config", `Reading ${fileName} from ${path}`);
-              const newConfig = JSON.parse(fs.readFileSync(filePath));
+    try {
+        // iterate over the paths and if a file exists, read it on top of the
+        // existing configuration object.
+        paths.forEach((path) => {
+            const filePath = normalizePath(path) + fileName;
+            if (fs.existsSync(filePath)) {
+                verbose("config", `Reading ${fileName} from ${path}`);
+                const newConfig = JSON.parse(fs.readFileSync(filePath));
 
-              // if any of the subobjects have a path element, update it to be relative to the current file
-              Object.values(newConfig).forEach((value) => {
-                  if (value.path) {
-                      value.path = normalizePath(getAbsolutePath(value.path, {relativeTo: path}));
-                  }
-              });
-              config = {...config, ...newConfig};
-              config.path = normalizePath(path); // add the path of the final config file read - for relative paths
-          }
-      });
-  } catch (err) {
-      throw new Error(`Error reading file "${fileName}": ${err.message}`);
-  }
+                // if any of the subobjects have a path element, update it to be relative to the current file
+                Object.values(newConfig).forEach((value) => {
+                    if (value.path) {
+                        value.path = normalizePath(getAbsolutePath(value.path, {relativeTo: path}));
+                    }
+                });
+                config = {...config, ...newConfig};
+                config.path = normalizePath(path); // add the path of the final config file read - for relative paths
+            }
+        });
+    } catch (err) {
+        throw new Error(`Error reading file "${fileName}": ${err.message}`);
+    }
 
-  return config;
+    return config;
 }
 
 function findConfigFile(paths, fileName) {
-  let foundFilePath = undefined;
+    let foundFilePath = undefined;
 
-  // iterate over the paths looking for the file, return the path to the last version found.
-  paths.forEach( (path) => {
-      const filePath = normalizePath(path) + fileName;
-      if (fs.existsSync(filePath)) {
-          foundFilePath = filePath;
-      }
-  });
+    // iterate over the paths looking for the file, return the path to the last version found.
+    paths.forEach( (path) => {
+        const filePath = normalizePath(path) + fileName;
+        if (fs.existsSync(filePath)) {
+            foundFilePath = filePath;
+        }
+    });
 
-  return foundFilePath;
+    return foundFilePath;
 }
 
 function assert(item, message) {
-  if (!item) {
-      fatal(message);
-  }
+    if (!item) {
+        fatal(message);
+    }
 }
 
 
 const getBarcodesInConfig = (config) => {
-  const barcodesDefined = new Set();
-  config.run.samples.forEach((s) => {
-      s.barcodes.forEach((b) => {
-          barcodesDefined.add(b);
-      })
-  })
-  return barcodesDefined;
+    const barcodesDefined = new Set();
+    config.run.samples.forEach((s) => {
+        s.barcodes.forEach((b) => {
+            barcodesDefined.add(b);
+        })
+    })
+    return barcodesDefined;
 }
 
 
@@ -90,33 +90,62 @@ const getBarcodesInConfig = (config) => {
  * This will overwrite any samples currently in the config.
  */
 function setBarcodesFromFile(config, barcodeFile) {
-  try {
-      verbose("config", `Reading sample to barcode mapping from ${barcodeFile}`);
-      const samples = dsv.csvParse(fs.readFileSync(barcodeFile).toString());
+    try {
+        verbose("config", `Reading sample to barcode mapping from ${barcodeFile}`);
+        const samples = dsv.csvParse(fs.readFileSync(barcodeFile).toString());
 
-      const sampleMap = samples.reduce( (sampleMap, d) => {
-          sampleMap[d.sample] = (d.sample in sampleMap ? [...sampleMap[d.sample], d.barcode] : [d.barcode]);
-          return sampleMap;
-      }, {});
-      if (config.run.samples.length) {
-        verbose("config", `Overriding existing barcode - sample name mapping`);
-      }
-      config.run.samples = Object.keys(sampleMap).map((d) => {
-          return {
-              'name': d,
-              'description': "",
-              'barcodes': sampleMap[d]
-          };
-      });
-  } catch (err) {
-      warn("Unable to read barcode to sample map file: " + barcodeFile)
-  }
+        let samplesKey = null;
+        let barcodesKey = null;
+        samples.columns.forEach(col => {
+            // trim() removes the BOM ('\uFEFF` used as a marker for byte order in UTF-8 files)
+            if (col.trim().toUpperCase().startsWith("SAMPLE")) {
+                samplesKey = col;
+            }
+            if (col.trim().toUpperCase().startsWith("BARCODE")) {
+                barcodesKey = col;
+            }
+        });
+
+        if (samplesKey == null) {
+            warn("No sample column found in barcode mapping file: " + barcodeFile);
+            return;
+        }
+
+        if (barcodesKey == null) {
+            warn("No barcode column found in barcode mapping file: " + barcodeFile);
+            return;
+        }
+
+
+        const sampleMap = samples.reduce( (sampleMap, d) => {
+//          sampleMap[d.sample] = (d.sample in sampleMap ? [...sampleMap[d.sample], d.barcode] : [d.barcode]);
+            sampleMap[d[samplesKey]] = (d[samplesKey] in sampleMap ?
+                [...sampleMap[samplesKey], d[barcodesKey]] : [d[barcodesKey]]);
+            return sampleMap;
+        }, {});
+
+        if (config.run.samples.length) {
+            verbose("config", `Overriding existing barcode - sample name mapping`);
+        }
+
+        config.run.samples = Object.keys(sampleMap).map((d) => {
+            return {
+                'name': d,
+                'description': "",
+                'barcodes': sampleMap[d]
+            };
+        });
+    } catch (err) {
+        warn(err);
+
+        warn("Unable to read barcode to sample map file: " + barcodeFile)
+    }
 }
 
 module.exports = {
-  readConfigFile,
-  findConfigFile,
-  assert,
-  getBarcodesInConfig,
-  setBarcodesFromFile
+    readConfigFile,
+    findConfigFile,
+    assert,
+    getBarcodesInConfig,
+    setBarcodesFromFile
 }
